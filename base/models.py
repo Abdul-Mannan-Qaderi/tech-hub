b7fa69d853163ccdf990f96e31edf286ed77934a
Fahad <fahadfateh1000@gmail.com>
2025-04-23 13:33:46 -0800

    work on models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+  # host
+  # toppc
+  name = models.CharField(max_length=255)
+  description = models.TextField(null=True, blank=True)
+  # participants
+  last_updated = models.DateTimeField(auto_now=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  
+  
+  def __str__(self):
+      return self.name
+  
