8dc261d96e19755f677d3d15b61107ff8403e795
Fahad <fahadfateh1000@gmail.com>
2025-04-23 02:35:56 -0800

    work on urls, views and templates

--- tech_hub/settings.py
+++ tech_hub/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tech_hub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
