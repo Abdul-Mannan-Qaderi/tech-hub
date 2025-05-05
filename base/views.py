96ee1a4ecc7fe21363fce7c354d66694bd23b03f
Fahad <fahadfateh1000@gmail.com>
2025-05-05 14:11:56 -0800

    finish profile update settings

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Room, Topic, Message
 from django.contrib.auth import authenticate, login, logout
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 def login_view(request):   
@@ -154,3 +154,19 @@ def delete_message(request, pk):
     message.delete()
     return redirect('home')
   return render(request, 'base/delete.html', {'obj': message})
+
+
+@login_required(login_url='/login')
+def update_user(request):
+  user = request.user
+  form = UserForm(instance=user)
+  
+  if request.method == 'POST':
+    form = UserForm(request.POST, instance=user)
+    if form.is_valid(): 
+      form.save()
+      return redirect('user-profile', pk=user.id)
+  context = {'form':form }
+  return render(request, 'base/update-user.html',context );
+ 
+ 
