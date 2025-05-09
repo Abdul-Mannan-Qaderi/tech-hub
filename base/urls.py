f33178e31499e84a6365f2b827058080e236c99e
Fahad <fahadfateh1000@gmail.com>
2025-05-09 14:36:58 +0430

    finish theme activity component

--- base/urls.py
+++ base/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('delete-message/<str:pk>/', views.delete_message, name='delete-message'),
     path('update-user/', views.update_user, name='update-user'),
     path('topics/', views.topicsPage, name='topics'),
+    path('activity/', views.activityPage, name='activity'),
 ]
